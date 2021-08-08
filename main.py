6c6c3be77fdef09826a11756b7797c3e46292694
Zike01 <zikekomakoma@gmail.com>
2021-08-08 15:43:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import pdfplumber
 import pyttsx3
 
 # Get the file name
-FILENAME = "Harry-Potter-and-the-Philosopher.pdf"
+PDF_FILE = "Harry-Potter-and-the-Philosopher.pdf"
 
 # Create a PDF file object
-pdfFileObj = open(FILENAME, 'rb')
+pdfFileObj = open(PDF_FILE, 'rb')
 
 # Create a PDF file Reader Object
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
@@ -16,10 +16,12 @@ pages = pdfReader.numPages
 
 # Initialize the pyttsx3 engine
 speaker = pyttsx3.init()
+
+# Set the rate of the speaker
 speaker.setProperty('rate', 150)
 
 # Create a pdfplumber object and loop through the pages
-with pdfplumber.open(FILENAME) as pdf:
+with pdfplumber.open(PDF_FILE) as pdf:
     text_list = []
     for i in range(0, pages):
         page = pdf.pages[i]
@@ -27,6 +29,9 @@ with pdfplumber.open(FILENAME) as pdf:
 
         for word in text:
             text_list.append(word)
-  
-speaker.save_to_file(' '.join(text_list), FILENAME.split('.')[0] + '.mp3')
+
+# Save audio to an mp3 file
+MP3_FILE = PDF_FILE.split('.')[0] + '.mp3'
+speaker.save_to_file(' '.join(text_list), MP3_FILE)
+    
 speaker.runAndWait()
