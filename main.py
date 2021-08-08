fa248ed570414b573d775912e07fa8d2a3495cd6
Zike01 <zikekomakoma@gmail.com>
2021-08-08 15:19:13 +0300

    Increase text-to-speech speaker rate

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pdfplumber
 import pyttsx3
 
 # Get the file name
-FILENAME = "harry-potter-sample.pdf"
+FILENAME = "Harry-Potter-and-the-Philosopher.pdf"
 
 # Create a PDF file object
 pdfFileObj = open(FILENAME, 'rb')
@@ -14,14 +14,21 @@ pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 # Get the number of pages
 pages = pdfReader.numPages
 
+# Initialize the pyttsx3 engine
+speaker = pyttsx3.init()
+speaker.setProperty('rate', 150)
+
 # Create a pdfplumber object and loop through the pages
 with pdfplumber.open(FILENAME) as pdf:
+    text_list = []
     for i in range(0, pages):
         page = pdf.pages[i]
-        text = page.extract_text()
-        print(text)
+        text = page.extract_text().split()
 
-        # Initialize the pyttsx3 engine
-        speaker = pyttsx3.init()
-        speaker.say(text)
-        speaker.runAndWait()
+        for word in text:
+            text_list.append(word)
+            
+    print(' '.join(text_list))
+  
+speaker.save_to_file(' '.join(text_list), FILENAME.split('.')[0] + '.mp3')
+speaker.runAndWait()
