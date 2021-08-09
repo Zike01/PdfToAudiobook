bffa92fc0e39fc4b3785082dec3d1b71d99d3d2f
Zike01 <zikekomakoma@gmail.com>
2021-08-09 16:59:50 +0300

    Update background color of window.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from tkinter import filedialog, messagebox
 
 #------------------------------- CONSTANTS -------------------------#
 BLUE = '#0275D8'
+BEIGE = '#F1CCAC'
 filename = None
 
 
@@ -64,32 +65,37 @@ def select_pdf():
 #------------------------------- UI SETUP -------------------------#
 window = Tk()
 window.title("PDF To Audiobook")
-window.config(padx=20, pady=20)
+window.config(padx=20, pady=20, bg=BEIGE)
 window.eval('tk::PlaceWindow . center')
 
-# Labels
-panel = Label(text="No file selected.")
-panel.grid(row=1, column=1)
+# IMAGE
+canvas = Canvas(width=80, height=80, bg=BEIGE, highlightthickness=0)
+pdf_icon = PhotoImage(file="assets/pdf-icon.png")
+canvas.create_image(40, 40, image=pdf_icon)
+canvas.grid(row=0, column=1, padx=30, pady=0)
 
-yeet = Label(text="YEet")
-yeet.grid(row=0, column=1)
+# LABELS
+panel = Label(text="No file selected.", bg=BEIGE)
+panel.grid(row=1, column=1, padx=30, pady=0)
 
-# Buttons
+# BUTTONS
 select_pdf_button = Button(text="Select PDF", bg=BLUE, fg="white", command=select_pdf)
-select_pdf_button.grid(row=2, column=1)
+select_pdf_button.grid(row=2, column=1, padx=30, pady=0)
 
 convert_button = Button(text="Convert to Audio", bg=BLUE, fg="white", command=lambda: convert_to_audio(pdf_file=filename, rate=slider.get()))
-convert_button.grid(row=2, column=2)
+convert_button.grid(row=2, column=2, padx=30, pady=0)
 
-# Slider
-speaker_rate = Label(text="Speaker Rate:")
-speaker_rate.grid(row=1, column=0)
+# SLIDER
+speaker_rate = Label(text="Speaker Rate:", bg=BEIGE)
+speaker_rate.grid(row=1, column=0, padx=30, pady=0)
 
 slider = Scale(window,
             from_=100,
             to=200,
-            orient='horizontal'
+            orient='horizontal',
+            highlightthickness= 0,
+            bg=BEIGE
 )
-slider.grid(row=2, column=0)
+slider.grid(row=2, column=0, padx=30, pady=0)
 
 window.mainloop()
