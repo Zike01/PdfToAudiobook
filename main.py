5b66c12a743aba96e11ed2df553541e1b16b8cc3
Zike01 <zikekomakoma@gmail.com>
2021-08-09 16:21:47 +0300

    Refactor main.py

--- main.py
+++ main.py
@@ -8,39 +8,43 @@ filename = None
 
 
 def convert_to_audio(pdf_file, rate):
-    # Create a PDF file object
-    pdfFileObj = open(pdf_file, 'rb')
+    if pdf_file:
+        # Create a PDF file object
+        pdfFileObj = open(pdf_file, 'rb')
 
-    # Create a PDF file Reader Object
-    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+        # Create a PDF file Reader Object
+        pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 
-    # Get the number of pages
-    pages = pdfReader.numPages
+        # Get the number of pages
+        pages = pdfReader.numPages
 
-    # Initialize the pyttsx3 engine
-    speaker = pyttsx3.init()
+        # Initialize the pyttsx3 engine
+        speaker = pyttsx3.init()
 
-    # Set the rate of the speaker
-    speaker.setProperty('rate', rate)
+        # Set the rate of the speaker
+        speaker.setProperty('rate', rate)
 
-    # Create a pdfplumber object and loop through the pages
-    with pdfplumber.open(pdf_file) as pdf:
-        text_list = []
-        for i in range(0, pages):
-            page = pdf.pages[i]
-            text = page.extract_text().split()
+        # Create a pdfplumber object and loop through the pages
+        with pdfplumber.open(pdf_file) as pdf:
+            text_list = []
+            for i in range(0, pages):
+                page = pdf.pages[i]
+                text = page.extract_text().split()
 
-            for word in text:
-                text_list.append(word)
+                for word in text:
+                    text_list.append(word)
 
-    # Save audio to an mp3 file
-    mp3_file = pdf_file.split('/')[-1].split('.')[0] + '.mp3'
-    speaker.save_to_file(' '.join(text_list), f"samples/mp3/{mp3_file}")  
-    speaker.runAndWait()
+        # Save audio to an mp3 file
+        mp3_file = pdf_file.split('/')[-1].split('.')[0] + '.mp3'
+        speaker.save_to_file(' '.join(text_list), f"samples/mp3/{mp3_file}")  
+        speaker.runAndWait()
+        
+        # Open messagebox once mp3 file is saved and close window once user clicks 'ok'
+        messagebox.showinfo(title="MP3 Saved", message=f"Successfully saved '{mp3_file}' to samples/mp3.")
+        window.destroy()
+    else:
+        messagebox.showerror(title="Error", message="No pdf file selected, please try again.")
     
-    # Open messagebox once mp3 file is saved and close window once user clicks 'ok'
-    messagebox.showinfo(title="MP3 Saved", message=f"{mp3_file} saved to samples/mp3.")
-    window.destroy()
     
     
 def select_pdf():
@@ -48,26 +52,11 @@ def select_pdf():
     Opens a dialog box for the user to select a PDF file.
     """
     global filename
-    filename = filedialog.askopenfilename(title='Select PDF', filetypes=[
-        ("PDF", "*.pdf"),
+    filename = filedialog.askopenfilename(title='Choose a file', filetypes=[
+        ("pdf file", "*.pdf"),
     ])
-    panel.configure(text=filename)
+    panel.configure(text=filename.split('/')[-1].split('.')[0])
     
-    # # Add convert button and slider button
-    # Slider
-    speaker_rate = Label(text="Set Speaker Rate:")
-    speaker_rate.pack()
-    
-    slider = Scale(window,
-                from_=100,
-                to=200,
-                orient='horizontal'
-    )
-    slider.pack()
-    
-    # Buttons
-    convert_button = Button(text="Convert to Audio",  command=lambda: convert_to_audio(pdf_file=filename, rate=slider.get()))
-    convert_button.pack()
 
 
 #------------------------------- UI SETUP -------------------------#
@@ -83,5 +72,22 @@ panel.pack()
 # Buttons
 select_pdf_button = Button(text="Select PDF", command=select_pdf)
 select_pdf_button.pack()
+
+
+# # Add convert button and slider button
+# Slider
+speaker_rate = Label(text="Set Speaker Rate:")
+speaker_rate.pack()
+
+slider = Scale(window,
+            from_=100,
+            to=200,
+            orient='horizontal'
+)
+slider.pack()
+
+# Buttons
+convert_button = Button(text="Convert to Audio",  command=lambda: convert_to_audio(pdf_file=filename, rate=slider.get()))
+convert_button.pack()
 
 window.mainloop()
